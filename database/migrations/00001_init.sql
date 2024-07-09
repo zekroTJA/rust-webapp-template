e791f55f8794fb93648150c82b456fcb0aa94b75
zekroTJA <riho@live.de>
2024-07-09 23:06:04 +0200

    add api_token database

--- database/migrations/00001_init.sql
+++ database/migrations/00001_init.sql
@@ -3,3 +3,15 @@ CREATE TABLE "user" (
     "registered_at" TIMESTAMPTZ NOT NULL,
     PRIMARY KEY ("id")
 );
+
+CREATE TABLE "api_token" (
+    "id" VARCHAR(20) NOT NULL,
+    "user_id" VARCHAR(40) NOT NULL,
+    "hash" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("user_id")
+        REFERENCES "user" ("id")
+        ON DELETE CASCADE
+)
